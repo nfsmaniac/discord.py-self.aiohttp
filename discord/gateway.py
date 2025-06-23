91a53e7627a219a269671fff92e38f931805ab3d
dolfies <jeyalfie47@gmail.com>
2025-06-23 14:29:08 -0400

    Remove unnecessary HELLO poll

--- discord/gateway.py
+++ discord/gateway.py
@@ -375,11 +375,7 @@ class DiscordWebSocket:
             ws.log_receive = ws.debug_log_receive
 
         client._connection._update_references(ws)
-
         _log.debug('Connected to %s.', gateway)
-
-        # Poll for Hello
-        await ws.poll_event()
 
         if not resume:
             await ws.identify()
