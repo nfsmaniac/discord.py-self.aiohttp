c660cbe9b5dae609cbd6d627d5c0e2011c3a6bfb
beerpsi <92439990+beer-psi@users.noreply.github.com>
2025-10-09 04:33:29 +0700

    Use compression.zstd for gateway compression on Python 3.14

--- discord/utils.py
+++ discord/utils.py
@@ -76,10 +76,14 @@ import typing
 import warnings
 import aiohttp
 import logging
-import zlib
 import struct
 import time
 import yarl
+
+if sys.version_info >= (3, 14):
+    import compression.zstd
+else:
+    import zlib
 
 try:
     import orjson  # type: ignore
@@ -1851,6 +1855,21 @@ if HAS_ZSTD:
         def __init__(self) -> None:
             decompressor = zstandard.ZstdDecompressor()
             self.context = decompressor.decompressobj()
+
+        def decompress(self, data: bytes, /) -> str | None:
+            # Each WS message is a complete gateway message
+            return self.context.decompress(data).decode('utf-8')
+
+    _ActiveDecompressionContext: Type[_DecompressionContext] = _ZstdDecompressionContext
+elif sys.version_info >= (3, 14):
+
+    class _ZstdDecompressionContext:
+        __slots__ = ('context',)
+
+        COMPRESSION_TYPE: str = 'zstd-stream'
+
+        def __init__(self) -> None:
+            self.context = compression.zstd.ZstdDecompressor()
 
         def decompress(self, data: bytes, /) -> str | None:
             # Each WS message is a complete gateway message
