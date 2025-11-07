581f9df26f400a04b72b2dcf9c33412e3260ce03
Michael H <michael@michaelhall.tech>
2025-11-07 18:57:01 -0500

    [Zstandard] Decompress even when discord doesn't encode size information

--- discord/utils.py
+++ discord/utils.py
@@ -89,20 +89,20 @@ except ModuleNotFoundError:
 else:
     HAS_ORJSON = True
 
+_ZSTD_SOURCE: Literal['zstandard', 'compression.zstd'] | None = None
 
 try:
     from zstandard import ZstdDecompressor  # type: ignore
 
-    _HAS_ZSTD = True
+    _ZSTD_SOURCE = 'zstandard'
 except ImportError:
     try:
         from compression.zstd import ZstdDecompressor  # type: ignore
+
+        _ZSTD_SOURCE = 'compression.zstd'
     except ImportError:
         import zlib
 
-        _HAS_ZSTD = False
-    else:
-        _HAS_ZSTD = True
 
 __all__ = (
     'oauth_url',
@@ -1851,7 +1851,7 @@ class IDGenerator:
         return b64encode(uuid).decode("utf-8")
 
 
-if HAS_ZSTD:
+if _ZSTD_SOURCE is not None:
 
     class _ZstdDecompressionContext:
         __slots__ = ('decompressor',)
@@ -1860,6 +1860,12 @@ if HAS_ZSTD:
 
         def __init__(self) -> None:
             self.decompressor = ZstdDecompressor()
+            if _ZSTD_SOURCE == 'zstandard':
+                # The default API for zstandard requires a size hint when
+                # the size is not included in the zstandard frame.
+                # This constructs an instance of zstandard.ZstdDecompressionObj
+                # which dynamically allocates a buffer, matching stdlib module's behavior.
+                self.decompressor = self.decompressor.decompressobj()
 
         def decompress(self, data: bytes, /) -> str | None:
             # Each WS message is a complete gateway message
