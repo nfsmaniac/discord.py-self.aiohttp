0b578f10e0b2b8ed3f23ddb3d39e9ecd9a3de078
n6ck <90283633+n6ck@users.noreply.github.com>
2025-11-12 03:52:54 +0000

    Change description to be optional when creating emoji

--- discord/guild.py
+++ discord/guild.py
@@ -27,7 +27,6 @@ from __future__ import annotations
 import copy
 from datetime import datetime
 from operator import attrgetter
-import unicodedata
 from typing import (
     Any,
     AsyncIterator,
@@ -3489,7 +3488,7 @@ class Guild(Hashable):
         self,
         *,
         name: str,
-        description: str,
+        description: str = MISSING,
         emoji: str,
         file: File,
         reason: Optional[str] = None,
@@ -3505,11 +3504,16 @@ class Guild(Hashable):
         Parameters
         -----------
         name: :class:`str`
-            The sticker name. Must be at least 2 characters.
+            The sticker name. Must be between 2 and 30 characters.
         description: :class:`str`
-            The sticker's description.
+            The sticker's description. Can be an empty string or a string between 2 and 100 characters.
+            Defaults to an empty string if not provided.
         emoji: :class:`str`
-            The name of a unicode emoji that represents the sticker's expression.
+            The emoji tag associated with the sticker. This corresponds to the
+            ``tags`` field in Discord's API, which is used for emoji autocomplete
+            and suggestion purposes. For correct rendering in Discord's UI, this
+            should ideally be a raw Unicode emoji or the string ID
+            of a custom emoji. Any string up to 200 characters is accepted.
         file: :class:`File`
             The file of the sticker to upload.
         reason: :class:`str`
@@ -3530,16 +3534,8 @@ class Guild(Hashable):
         payload = {
             'name': name,
             'description': description or '',
+            'tags': emoji,
         }
-
-        try:
-            emoji = unicodedata.name(emoji)
-        except TypeError:
-            pass
-        else:
-            emoji = emoji.replace(' ', '_')
-
-        payload['tags'] = emoji
 
         data = await self._state.http.create_guild_sticker(self.id, payload, file, reason)
         return self._state.store_sticker(self, data)
