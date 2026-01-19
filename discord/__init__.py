661c84ba1b9709ad439c7c257cd4522288426a70
dolfies <jeyalfie47@gmail.com>
2026-01-18 21:25:28 -0500

    Version bump for development

--- discord/__init__.py
+++ discord/__init__.py
@@ -12,7 +12,7 @@ __title__ = 'discord.py-self'
 __author__ = 'Dolfies'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-present Rapptz and 2021-present Dolfies'
-__version__ = '2.1.0'
+__version__ = '2.2.0a'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
@@ -94,7 +94,7 @@ class _VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: _VersionInfo = _VersionInfo(major=2, minor=1, micro=0, releaselevel='final', serial=0)
+version_info: _VersionInfo = _VersionInfo(major=2, minor=2, micro=0, releaselevel='alpha', serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
