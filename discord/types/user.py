Rapptz/discord.py

Add support for user collectibles

discord/types/user.py
@@ -27,9 +27,36 @@ from typing import Literal, Optional, TypedDict
 from typing_extensions import NotRequired
 
 
-class AvatarDecorationData(TypedDict):
+PremiumType = Literal[0, 1, 2, 3]
+NameplatePallete = Literal['crimson', 'berry', 'sky', 'teal', 'forest', 'bubble_gum', 'violet', 'cobalt', 'clover']
+
+
+class _UserSKU(TypedDict):
     asset: str
     sku_id: Snowflake
+
+
+AvatarDecorationData = _UserSKU
+
+
+class PrimaryGuild(TypedDict):
+    identity_guild_id: Optional[int]
+    identity_enabled: Optional[bool]
+    tag: Optional[str]
+    badge: Optional[str]
+
+
+class Collectible(_UserSKU):
+    label: str
+    expires_at: Optional[str]
+
+
+class NameplateCollectible(Collectible):
+    palette: str
+
+
+class UserCollectibles(TypedDict):
+    nameplate: NameplateCollectible
 
 
 class PartialUser(TypedDict):
@@ -39,9 +66,8 @@ class PartialUser(TypedDict):
     avatar: Optional[str]
     global_name: Optional[str]
     avatar_decoration_data: NotRequired[AvatarDecorationData]
-
-
-PremiumType = Literal[0, 1, 2, 3]
+    primary_guild: NotRequired[PrimaryGuild]
+    collectibles: NotRequired[UserCollectibles]
 
 
 class User(PartialUser, total=False):
@@ -54,10 +80,3 @@ class User(PartialUser, total=False):
     flags: int
     premium_type: PremiumType
     public_flags: int
-
-
-class PrimaryGuild(TypedDict):
-    identity_guild_id: Optional[int]
-    identity_enabled: Optional[bool]
-    tag: Optional[str]
-    badge: Optional[str]

nfsmaniac/discord.py-self.aiohttp

discord/types/user.py
@@ -30,6 +30,31 @@ from typing_extensions import NotRequired
 from .integration import ConnectionIntegration
 from .snowflake import Snowflake
 
+PremiumType = Literal[0, 1, 2, 3]
+NameplatePallete = Literal['crimson', 'berry', 'sky', 'teal', 'forest', 'bubble_gum', 'violet', 'cobalt', 'clover']
+
+
+class _UserSKU(TypedDict):
+    asset: str
+    sku_id: Snowflake
+
+
+class AvatarDecorationData(_UserSKU):
+    expires_at: Optional[int]
+
+
+class Collectible(_UserSKU):
+    label: str
+    expires_at: Optional[str]
+
+
+class NameplateCollectible(Collectible):
+    palette: str
+
+
+class UserCollectibles(TypedDict):
+    nameplate: NameplateCollectible
+
 
 class PartialUser(TypedDict):
     id: Snowflake
@@ -43,6 +68,7 @@ class PartialUser(TypedDict):
     global_name: Optional[str]
     primary_guild: NotRequired[Optional[PrimaryGuild]]
     display_name_styles: Optional[DisplayNameStyle]
+    collectibles: NotRequired[Optional[UserCollectibles]]
 
 
 ConnectionType = Literal[
@@ -107,12 +133,6 @@ class DisplayNameStyle(TypedDict):
 
 class UserWithToken(User):
     token: str
-
-
-class AvatarDecorationData(TypedDict):
-    asset: str
-    sku_id: Snowflake
-    expires_at: Optional[int]
 
 
 class PrimaryGuild(TypedDict):